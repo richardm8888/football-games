4bdbf217b53fb60a28af367cd7a104d0a774ca71
Richard Moss <richy8888@hotmail.com>
2024-02-09 20:47:11 +0000

    Sitemap

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -12,32 +12,24 @@ export default async function handler(
   response: any
 ) {;
     const { body } = request;
-    let currentUrls: any = [];
     const xml = fs.readFileSync(__dirname + '/sitemap_template.xml');
     let result: any = await parseXml(xml);
-    currentUrls = result.urlset.url;
 
     const client = createClient();
     client.connect();
 
-    const { rows } = await client.query(`SELECT * FROM sitemap WHERE loc = $1`, [body?.data?.slug]);
-
-    if (rows.length === 0) {
-        let url = body?.data?.slug;
-        if (body?.data.__typename === 'Article') {
-            url = `/${(body?.data?.category?.slug ?? '')}/${url}`;
-        }
-        await client.query(`INSERT INTO sitemap (loc, lastmod, priority) VALUES ($1, $2, $3)`, [body?.data?.slug, body?.data.updatedAt, 0.8]);
-        rows.push({ loc: body?.data?.slug, lastmod: body?.data.updatedAt, changefreq: 'daily', priority: 0.8 });
-    }
-
+    const { rows } = await client.query(`SELECT * FROM sitemap`);
     client.end();
 
-    result.urlset.url = rows;
+    result.urlset.url = rows.map((row: any) => {
+        row.loc = `https://www.football-connect.co.uk/${row.loc}`;
+        return row;
+    });
     const builder = new Builder();
     const newxml = builder.buildObject(result);
    
-    return response.status(200).json({ new: newxml, slug: body?.data.slug, type: body?.data.__typename, updated_at: body?.data.updatedAt });
+    response.setHeader('Content-Type', 'application/xml');
+    return response.status(200).send(newxml);
 }
 
 
