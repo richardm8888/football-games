de54c3be675d0c25ec9d1765c5b3fc3e573a192d
Richard Moss <richy8888@hotmail.com>
2024-02-20 18:04:08 +0000

    Save movie answers differently

--- src/components/Game/MovieConnect/helpers.ts
+++ src/components/Game/MovieConnect/helpers.ts
@@ -1,13 +1,13 @@
 export function saveGuesses(difficulty: string, guesses: string[]) {
-    localStorage.setItem('guesses_' + difficulty, JSON.stringify({ date: new Date(), guesses }));
+    localStorage.setItem('movie_guesses_' + difficulty, JSON.stringify({ date: new Date(), guesses }));
 }
 
 export function getGuesses(difficulty: string) {
-    const guesses = localStorage.getItem('guesses_' + difficulty);
+    const guesses = localStorage.getItem('movie_guesses_' + difficulty);
     if (guesses) {
         const parsed = JSON.parse(guesses);
         if (new Date(parsed.date) < new Date(new Date().setHours(0,0,0,0))) {
-            localStorage.removeItem('guesses_' + difficulty);
+            localStorage.removeItem('movie_guesses_' + difficulty);
             return [];
         }
         return parsed.guesses;
@@ -17,15 +17,15 @@ export function getGuesses(difficulty: string) {
 }
 
 export function saveIncorrectGuesses(difficulty: string, guesses: string[]) {
-    localStorage.setItem('incorrectGuesses_' + difficulty, JSON.stringify({ date: new Date(), guesses }));
+    localStorage.setItem('movie_incorrectGuesses_' + difficulty, JSON.stringify({ date: new Date(), guesses }));
 }
 
 export function getIncorrectGuesses(difficulty: string) {
-    const guesses = localStorage.getItem('incorrectGuesses_' + difficulty);
+    const guesses = localStorage.getItem('movie_incorrectGuesses_' + difficulty);
     if (guesses) {
         const parsed = JSON.parse(guesses);
         if (new Date(parsed.date) < new Date(new Date().setHours(0,0,0,0))) {
-            localStorage.removeItem('incorrectGuesses_' + difficulty);
+            localStorage.removeItem('movie_incorrectGuesses_' + difficulty);
             return [];
         }
         return parsed.guesses;
@@ -41,15 +41,15 @@ export interface CorrectGuess {
 }
 
 export function saveCorrectGuesses(difficulty: string, correctGuesses: CorrectGuess[]) {
-    localStorage.setItem('correctGuesses_' + difficulty, JSON.stringify({ date: new Date(), guesses: correctGuesses}));
+    localStorage.setItem('movie_correctGuesses_' + difficulty, JSON.stringify({ date: new Date(), guesses: correctGuesses}));
 }
 
 export function getCorrectGuesses(difficulty: string) {
-    const correctGuesses = localStorage.getItem('correctGuesses_' + difficulty);
+    const correctGuesses = localStorage.getItem('movie_correctGuesses_' + difficulty);
     if (correctGuesses) {
         const parsed = JSON.parse(correctGuesses);
         if (new Date(parsed.date) < new Date(new Date().setHours(0,0,0,0))) {
-            localStorage.removeItem('correctGuesses_' + difficulty);
+            localStorage.removeItem('movie_correctGuesses_' + difficulty);
             return [];
         }
         return parsed.guesses;
